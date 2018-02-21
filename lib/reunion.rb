08c9888f1150151c8559d8c594a1955d43683e90
adam-conway <adam.n.conway@gmail.com>
2018-02-21 09:47:05 -0700

    Finishing iteration 3

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,13 @@
 class Reunion
+  attr_reader :location,
+              :activities
+  def initialize(location = 'nowhere', activity = 'nothing')
+    @location = location
+    @activities = []
+    @activities << activity
+  end
+
+  def add_activity(activity)
+    @activities << activity
+  end
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -2,6 +2,49 @@ require 'minitest'
 require 'minitest/test'
 require 'minitest/autorun'
 require './lib/reunion'
+require './lib/activity'
 
 class ReunionTest < Minitest::Test
+  def test_it_exists
+    reunion = Reunion.new
+
+    assert_instance_of Reunion, reunion
+  end
+
+  def test_it_has_location
+    reunion = Reunion.new('beach')
+
+    assert_equal 'beach', reunion.location
+  end
+
+  def test_it_has_activities
+    participants = {
+      'Adam': 300,
+      'Kelly': 150,
+      'Matt': 450
+    }
+    activity = Activity.new('hiking', participants)
+    reunion = Reunion.new('beach', activity)
+
+    assert_equal 'hiking', reunion.activities.first.name
+    assert_equal [:Adam, :Kelly, :Matt], reunion.activities.first.people.keys
+  end
+
+  def test_adding_activity
+    participants = {
+      'Adam': 300,
+    }
+    activity = Activity.new('hiking', participants)
+    reunion = Reunion.new('beach', activity)
+
+    participants2 = {
+      'Kelly': 150,
+      'Matt': 450
+    }
+    activity2 = Activity.new('surfing', participants2)
+    reunion.add_activity(activity2)
+
+    assert_equal 2, reunion.activities.length
+    assert_equal 'surfing', reunion.activities.last.name
+  end
 end
