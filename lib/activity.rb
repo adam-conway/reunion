35bf922dfe841f64e1a11772979ebdd2df6cad9e
adam-conway <adam.n.conway@gmail.com>
2018-02-21 09:17:52 -0700

    Finishing iteration 1

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,17 @@
 class Activity
+  attr_reader :name,
+              :people
+
+  def initialize(name = 'generic activity', people = 'nobody')
+    @name = name
+    @people = people
+  end
+
+  def add_participant(hash)
+    @people[hash.keys.first] = hash.values.first
+  end
+
+  def sum_cost
+    @people.values.sum
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,50 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_it_exists
+    activity = Activity.new
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_it_has_a_name
+    activity = Activity.new('hiking')
+
+    assert_equal 'hiking', activity.name
+  end
+
+  def test_it_has_participants
+    participants = {
+      'Adam': 300,
+      'Kelly': 150
+    }
+    activity = Activity.new('hiking', participants)
+
+    assert_equal 'hiking', activity.name
+    assert_equal [:Adam, :Kelly], activity.people.keys
+    assert_equal [300, 150], activity.people.values
+    assert_equal 300, activity.people[:Adam]
+  end
+
+  def test_adding_participants_to_activity
+    participants = {
+      'Adam': 300,
+      'Kelly': 150
+    }
+    activity = Activity.new('hiking', participants)
+    activity.add_participant('Matt': 500)
+
+    assert_equal [:Adam, :Kelly, :Matt], activity.people.keys
+    assert_equal [300, 150, 500], activity.people.values
+  end
+
+  def test_summing_total_cost_of_activity
+    participants = {
+      'Adam': 300,
+      'Kelly': 150
+    }
+    activity = Activity.new('hiking', participants)
+
+    assert_equal 450, activity.sum_cost
+  end
 end
